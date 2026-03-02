f54cd93657d2786e1af3ff73980c3b965114cf0a
Rohn Roy <rohn@faircodetech.com>
2026-03-02 13:50:22 +0530

    Production settings

--- asgi.py
+++ asgi.py
@@ -1,10 +1,11 @@
 import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_project.settings")
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 import chat.routing
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_project.settings")
 
 application = ProtocolTypeRouter(
     {
